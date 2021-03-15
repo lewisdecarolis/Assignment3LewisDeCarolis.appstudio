4a490fc22630b2eeea91d8294bf12a78ec0050dc
lewisdecarolis <lsd81882@creighton.edu>
2021-03-14 22:04:04 -0500

    passwords

---
+++ forms/forReverseStates/forReverseStates.js
@@ -0,0 +1,14 @@
+/*
+//Declares array
+let states = ["Nebraska", "Iowa", "Minnesota", "South Dakota", "North Dakota", "Kansas", "Oklahoma", "Texas"] 
+
+//Reverses the order of states with method
+reverseStates = states.reverse()
+
+//Loop to console each index on its own line
+for(i = 0; i < reverseStates.length; i++)
+        console.log(reverseStates[i])
+*/
+
+
+
---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,20 @@
+/*
+//Declare Vatiables
+let password1 = prompt("Enter your password")
+let password2 = prompt("Enter your password again")
+let similar = ""
+
+//Function to compare password 1 and 2
+function compare(pass1, pass2){
+let similar = (pass1 ==  pass2)
+if (similar)
+console.log("Similar")
+else
+console.log("Not similar")
+
+return similar
+}
+
+//Log the function for the developer to see
+console.log(`${password1} and ${password2} is ${compare(password1, password2)}`)
+*/
